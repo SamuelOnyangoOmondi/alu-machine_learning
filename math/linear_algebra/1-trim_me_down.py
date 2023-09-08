f5525322c268ba4bc60354141bc684162c9b240b
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-08 16:10:41 +0300

    Update 1-trim_me_down.py

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 = arr[0:2]
-arr2 = arr[-5:]
-arr3 = arr[1:6]
+matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = []
+for row in matrix:the_middle.append(row[2:4])
+print("The middle columns of the matrix are: {}".format(the_middle))
+The middle columns of the matrix are: [[9, 4], [7, 3], [4, 6]
