7e8eca0a4aa07de074f501420554301471ac46ba
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-08 17:16:49 +0300

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to determine the shape of a given matrix.
+"""
+
 def matrix_shape(matrix):
+    """
+    Returns the shape of a matrix as a list of integers.
+    
+    Parameters:
+    - matrix: The matrix whose shape is to be determined.
+
+    Returns:
+    - A list of integers representing the shape of the matrix.
+    """
     shape = []
     while type(matrix) is list:  # check if list
         shape.append(len(matrix))
