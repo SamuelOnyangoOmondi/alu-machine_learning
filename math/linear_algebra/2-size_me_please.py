d406e9f3c026ad7375e701b8fe75a13e2673fc49
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-08 17:11:04 +0300

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
     shape = []
-    while type(matrix) is list:#check if list
+    while type(matrix) is list:  #check if list
         shape.append(len(matrix))
-        matrix = matrix[0]#next dimension
+        matrix = matrix[0]  #next dimension
     return shape
