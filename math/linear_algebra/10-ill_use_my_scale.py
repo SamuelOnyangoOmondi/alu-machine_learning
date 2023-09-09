2cc2c85354329683d9c2f9801b20c4d6c31d89df
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 13:02:23 +0300

    Create 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy ndarray as a tuple of integers.
+    
+    Parameters:
+    - matrix: numpy ndarray
+
+    Returns:
+    - Tuple of integers representing the shape of the matrix.
+    """
+    return matrix.shape
+
+# Testing the function
+if __name__ == "__main__":
+    import numpy as np
+
+    mat1 = np.array([1, 2, 3, 4, 5, 6])
+    mat2 = np.array([])
+    mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                     [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+
+    print(np_shape(mat1))  # Expected: (6,)
+    print(np_shape(mat2))  # Expected: (0,)
+    print(np_shape(mat3))  # Expected: (2, 2, 5)
