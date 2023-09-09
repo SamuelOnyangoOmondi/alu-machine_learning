468814ca444f8caa7e333a194be1fddf0a2bd271
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 12:24:56 +0300

    Update 8-ridin_bareback.py

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,7 +4,6 @@
 This module provides functionality for multiplying two 2D matrices.
 """
 
-
 def mat_mul(mat1, mat2):
     """
     Multiplies two 2D matrices.
@@ -20,7 +19,6 @@ def mat_mul(mat1, mat2):
     # Number of columns in mat1 must equal number of rows in mat2
     if len(mat1[0]) != len(mat2):
         return None
-
     # Initialize the result matrix with zeros
     result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
 
@@ -28,8 +26,8 @@ def mat_mul(mat1, mat2):
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
-            result[i][j] += mat1[i][k] * mat2[k][j]
-            return result
+                result[i][j] += mat1[i][k] * mat2[k][j]          
+    return result
 
 
 # Testing the function
