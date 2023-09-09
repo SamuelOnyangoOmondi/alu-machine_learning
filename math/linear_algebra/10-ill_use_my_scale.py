a5b4de142c8d111f4c6ef98a5a15258d7c180f42
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 13:41:40 +0300

    Update 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -10,7 +10,7 @@ def np_shape(matrix):
 # mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
 #                  [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
 
-# If you do receive the arrays from another source, you can still call the function
-# print(np_shape(mat1))  
-# print(np_shape(mat2))  
-# print(np_shape(mat3))  
+# If you do receive the arrays call the function
+# print(np_shape(mat1))
+# print(np_shape(mat2))
+# print(np_shape(mat3))
