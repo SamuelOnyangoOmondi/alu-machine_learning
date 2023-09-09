241c5df63931f9775a7cc0077c8bbbc95e96eead
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 10:20:31 +0300

    Create 6-howdy_partner.py

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two lists.
+
+    Parameters:
+    - arr1: First list of ints/floats.
+    - arr2: Second list of ints/floats.
+
+    Returns:
+    - A new list that represents the concatenation of the two input lists.
+    """
+    return arr1 + arr2
+
+# Testing the function
+if __name__ == "__main__":
+    arr1 = [1, 2, 3, 4, 5]
+    arr2 = [6, 7, 8]
+    print(cat_arrays(arr1, arr2))
+    print(arr1)
+    print(arr2)
