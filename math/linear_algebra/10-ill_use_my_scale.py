e8606285dcdf4ad546dcb0b2bdbc41a6c27f3706
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 13:18:38 +0300

    Update 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
-import numpy as np
-
 def np_shape(matrix):
-    """Return the shape of a numpy ndarray."""
-    return matrix.shape
+    """Return the shape of a simulated numpy ndarray."""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] if matrix else []
+    return tuple(shape)
 
 # Test cases
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
-mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+mat1 = [1, 2, 3, 4, 5, 6]
+mat2 = []
+mat3 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+        [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]]
 
 print(np_shape(mat1))  # Expected output: (6,)
 print(np_shape(mat2))  # Expected output: (0,)
