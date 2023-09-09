90d8335eee48d4bc46e6f80bad1b33250842b096
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 13:39:26 +0300

    Update 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-import numpy as np
-
 def np_shape(matrix):
     return matrix.shape
 
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
-mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
-print(np_shape(mat1))  # Expected output: (6,)
-print(np_shape(mat2))  # Expected output: (0,)
-print(np_shape(mat3))  # Expected output: (2, 2, 5)
+# Assuming you receive the numpy arrays from another source
+# Commenting out the following lines since you can't use np.array here
+# mat1 = np.array([1, 2, 3, 4, 5, 6])
+# mat2 = np.array([])
+# mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+#                  [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+
+# If you do receive the arrays from another source, you can still call the function
+# print(np_shape(mat1))  
+# print(np_shape(mat2))  
+# print(np_shape(mat3))  
