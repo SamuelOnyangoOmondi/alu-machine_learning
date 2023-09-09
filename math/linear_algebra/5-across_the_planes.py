ada8e25f43a616656d9707698e5fbceb4719e27b
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 10:15:01 +0300

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,6 +4,7 @@
 Module provides functionality to add two 2D matrices element-wise.
 """
 
+
 def add_matrices2D(mat1, mat2):
     """
     Adds two 2D matrices element-wise.
@@ -17,7 +18,9 @@ def add_matrices2D(mat1, mat2):
       If matrices are of different shapes, return None.
     """
     # Check if the matrices have the same shape
-    if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
+    if len(mat1) != len(mat2) or any(
+        len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)
+    ):
         return None
 
     # Compute the sum of the matrices element-wise
