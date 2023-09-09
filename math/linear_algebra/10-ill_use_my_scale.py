4aedc37c65cd519c6604cd76d186ac3e0f0b1a72
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 13:08:14 +0300

    Update 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
 
-def np_shape(matrix):
-    """
-    Returns the shape of a numpy ndarray as a tuple of integers.
-    
-    Parameters:
-    - matrix: numpy ndarray
+import numpy as np
 
-    Returns:
-    - Tuple of integers representing the shape of the matrix.
-    """
+def np_shape(matrix):
+    """Return the shape of a numpy ndarray."""
     return matrix.shape
 
-# Testing the function
-if __name__ == "__main__":
-    import numpy as np
+# Test cases
+mat1 = np.array([1, 2, 3, 4, 5, 6])
+mat2 = np.array([])
+mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
 
-    mat1 = np.array([1, 2, 3, 4, 5, 6])
-    mat2 = np.array([])
-    mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                     [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
-
-    print(np_shape(mat1))  # Expected: (6,)
-    print(np_shape(mat2))  # Expected: (0,)
-    print(np_shape(mat3))  # Expected: (2, 2, 5)
+print(np_shape(mat1))  # Expected output: (6,)
+print(np_shape(mat2))  # Expected output: (0,)
+print(np_shape(mat3))  # Expected output: (2, 2, 5)
