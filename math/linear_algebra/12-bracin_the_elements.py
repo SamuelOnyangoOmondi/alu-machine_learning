7eda0ab02095d8fc1db2d74edcb3ffe94e9bee7a
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 14:25:58 +0300

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
+def elementwise_operation(op, mat1, mat2):
+    if isinstance(mat2, (int, float)):  # Check if mat2 is a scalar
+        return list(map(lambda row: list(map(op, row, [mat2]*len(row))), mat1))
+    return list(map(lambda row1, row2: list(map(op, row1, row2)), mat1, mat2))
+
 def np_elementwise(mat1, mat2):
     """
     Perform element-wise addition, subtraction, multiplication, and division.
     """
-    
-    # Check if mat2 is a scalar
-    if isinstance(mat2, (int, float)):
-        add = [[i + mat2 for i in row] for row in mat1]
-        sub = [[i - mat2 for i in row] for row in mat1]
-        mul = [[i * mat2 for i in row] for row in mat1]
-        div = [[i / mat2 for i in row] for row in mat1]
-    else:
-        add = [[i + j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
-        sub = [[i - j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
-        mul = [[i * j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
-        div = [[i / j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    add = elementwise_operation(lambda x, y: x + y, mat1, mat2)
+    sub = elementwise_operation(lambda x, y: x - y, mat1, mat2)
+    mul = elementwise_operation(lambda x, y: x * y, mat1, mat2)
+    div = elementwise_operation(lambda x, y: x / y, mat1, mat2)
     
     return add, sub, mul, div
 
