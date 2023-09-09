69c4e42fb3cf7a470e0c2dfaef5504ea87436ac0
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 14:45:16 +0300

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 def np_elementwise(mat1, mat2):
-    """
-    Perform element-wise addition, subtraction, multiplication, and division.
-    """
-    
-    # Convert scalar mat2 to matrix form if necessary
-    mat2 = [mat2]*len(mat1) if not hasattr(mat2[0], '__len__') else mat2
-    
-    add = [[i + j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
-    sub = [[i - j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
-    mul = [[i * j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
-    div = [[i / j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    """Perform element-wise operations on mat1 and mat2"""
+    # Element-wise addition
+    add = np.add(mat1, mat2)
+    # Element-wise subtraction
+    sub = np.subtract(mat1, mat2)
+    # Element-wise multiplication
+    mul = np.multiply(mat1, mat2)
+    # Element-wise division (using numpy's division which handles divide by zero gracefully by returning 'inf')
+    div = np.divide(mat1, mat2)
     
     return add, sub, mul, div
 
-# Given test cases
-mat1 = [[11, 22, 33], [44, 55, 66]]
-mat2 = [[1, 2, 3], [4, 5, 6]]
+mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+mat2 = np.array([[1, 2, 3], [4, 5, 6]])
 
 print(mat1)
 print(mat2)
 add, sub, mul, div = np_elementwise(mat1, mat2)
 print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
 
-add, sub, mul, div = np_elementwise(mat1, [2, 2, 2])
+add, sub, mul, div = np_elementwise(mat1, 2)
 print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
