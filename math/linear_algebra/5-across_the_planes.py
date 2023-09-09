b7ab9a43f2197da3ea5832fa8b09892f07822354
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 10:09:13 +0300

    Create 5-across_the_planes.py

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+def add_matrices2D(mat1, mat2):
+    """
+    Adds two 2D matrices element-wise.
+    
+    Parameters:
+    - mat1: First 2D matrix of ints/floats.
+    - mat2: Second 2D matrix of ints/floats.
+
+    Returns:
+    - A new 2D matrix that represents the sum of the two input matrices element-wise.
+      If the input matrices are of different shapes, return None.
+    """
+    # Check if the matrices have the same shape
+    if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
+        return None
+
+    # Compute the sum of the matrices element-wise
+    result = []
+    for i in range(len(mat1)):
+        result_row = [mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))]
+        result.append(result_row)
+
+    return result
+
+# Testing the function
+if __name__ == "__main__":
+    mat1 = [[1, 2], [3, 4]]
+    mat2 = [[5, 6], [7, 8]]
+    print(add_matrices2D(mat1, mat2))
+    print(mat1)
+    print(mat2)
+    print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
