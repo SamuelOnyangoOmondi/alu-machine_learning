ed7bad04284adb0c981a24469c88fe2783393df7
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 14:58:51 +0300

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
 def np_elementwise(mat1, mat2):
-    """Perform element-wise operations on mat1 and mat2"""
+    """Perform element-wise operations on mat1 and mat2."""
     
-    def elementwise_operation(x, y, operation):
+    def operate(x, y, operation):
         return list(map(operation, x, y))
-    
-    # Transform scalar mat2 to matrix form if needed
-    mat2_matrix = [[mat2]*len(mat1[0])]*len(mat1) if not isinstance(mat2[0], list) else mat2
 
-    add = list(map(lambda x, y: elementwise_operation(x, y, lambda a, b: a + b), mat1, mat2_matrix))
-    sub = list(map(lambda x, y: elementwise_operation(x, y, lambda a, b: a - b), mat1, mat2_matrix))
-    mul = list(map(lambda x, y: elementwise_operation(x, y, lambda a, b: a * b), mat1, mat2_matrix))
-    div = list(map(lambda x, y: elementwise_operation(x, y, lambda a, b: a / b), mat1, mat2_matrix))
+    mat2_broadcast = [operate(row, [mat2]*len(row), lambda a, b: b) if isinstance(mat2, int) else mat2 for row in mat1]
+
+    add = operate(mat1, mat2_broadcast, lambda x, y: operate(x, y, lambda a, b: a + b))
+    sub = operate(mat1, mat2_broadcast, lambda x, y: operate(x, y, lambda a, b: a - b))
+    mul = operate(mat1, mat2_broadcast, lambda x, y: operate(x, y, lambda a, b: a * b))
+    div = operate(mat1, mat2_broadcast, lambda x, y: operate(x, y, lambda a, b: a / b))
 
     return add, sub, mul, div
 
