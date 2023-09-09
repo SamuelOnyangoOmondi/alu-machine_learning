1034649477b8515919c9d10540d26b8061551f1d
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 14:21:28 +0300

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -4,10 +4,18 @@ def np_elementwise(mat1, mat2):
     """
     Perform element-wise addition, subtraction, multiplication, and division.
     """
-    add = mat1 + mat2
-    sub = mat1 - mat2
-    mul = mat1 * mat2
-    div = mat1 / mat2
+    
+    # Check if mat2 is a scalar
+    if isinstance(mat2, (int, float)):
+        add = [[i + mat2 for i in row] for row in mat1]
+        sub = [[i - mat2 for i in row] for row in mat1]
+        mul = [[i * mat2 for i in row] for row in mat1]
+        div = [[i / mat2 for i in row] for row in mat1]
+    else:
+        add = [[i + j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+        sub = [[i - j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+        mul = [[i * j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+        div = [[i / j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
     
     return add, sub, mul, div
 
