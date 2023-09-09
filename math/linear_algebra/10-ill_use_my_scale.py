c80e1659029c0cda1fd3ff8ea8f7c609788771db
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-09 13:23:30 +0300

    Update 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
 def np_shape(matrix):
-    """Return the shape of a simulated numpy ndarray using recursion."""
-    if not isinstance(matrix, list):
-        return ()
-    return (len(matrix),) + np_shape(matrix[0]) if matrix else ()
+    """Return the shape of a numpy ndarray."""
+    return matrix.shape
 
 # Test cases
-mat1 = [1, 2, 3, 4, 5, 6]
-mat2 = []
-mat3 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-        [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]]
+import numpy as np
+mat1 = np.array([1, 2, 3, 4, 5, 6])
+mat2 = np.array([])
+mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
 
 print(np_shape(mat1))  # Expected output: (6,)
 print(np_shape(mat2))  # Expected output: (0,)
