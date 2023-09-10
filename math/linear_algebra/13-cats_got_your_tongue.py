285cc636dd4122c0056b702749d83fba5e71e954
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-10 12:05:59 +0300

    Update 13-cats_got_your_tongue.py

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -16,11 +16,23 @@ def np_cat(mat1, mat2, axis=0):
     """
     return np.concatenate((mat1, mat2), axis=axis)
 
-# Testing the function
-mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-mat2 = np.array([[1, 2, 3], [4, 5, 6]])
-mat3 = np.array([[7], [8]])
+def test_np_cat():
+    # Define test matrices
+    mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+    mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+    mat3 = np.array([[7], [8]])
+    
+    # Expected results
+    expected_0 = np.array([[11, 22, 33], [44, 55, 66], [1, 2, 3], [4, 5, 6]])
+    expected_1 = np.array([[11, 22, 33, 1, 2, 3], [44, 55, 66, 4, 5, 6]])
+    expected_2 = np.array([[11, 22, 33, 7], [44, 55, 66, 8]])
+    
+    # Check the results
+    assert (np_cat(mat1, mat2) == expected_0).all()
+    assert (np_cat(mat1, mat2, axis=1) == expected_1).all()
+    assert (np_cat(mat1, mat3, axis=1) == expected_2).all()
 
-print(np_cat(mat1, mat2))        # Default is axis=0
-print(np_cat(mat1, mat2, axis=1))
-print(np_cat(mat1, mat3, axis=1))
+    print("OK")
+
+# Run the tests
+test_np_cat()
