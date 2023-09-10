a301bc3c8e1ba2f07f618121070dac202e5d9049
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-10 11:59:53 +0300

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+def operate(r1, r2):
+    return (
+        list(map(lambda x, y: x + y, r1, r2)),
+        list(map(lambda x, y: x - y, r1, r2)),
+        list(map(lambda x, y: x * y, r1, r2)),
+        list(map(lambda x, y: x / y, r1, r2))
+    )
+
 def np_elementwise(mat1, mat2):
     """
     Perform element-wise operations on two matrices.
@@ -12,16 +20,15 @@ def np_elementwise(mat1, mat2):
     - tuple: A tuple containing matrices resulting from element-wise sum, difference, product, and quotient
     """
     
-    # Element-wise operations using list comprehension
-    add = lambda m1, m2: [[i+j for i, j in zip(r1, r2)] for r1, r2 in zip(m1, m2)]
-    sub = lambda m1, m2: [[i-j for i, j in zip(r1, r2)] for r1, r2 in zip(m1, m2)]
-    mul = lambda m1, m2: [[i*j for i, j in zip(r1, r2)] for r1, r2 in zip(m1, m2)]
-    div = lambda m1, m2: [[i/j for i, j in zip(r1, r2)] for r1, r2 in zip(m1, m2)]
-    
     # Check if mat2 is a scalar
     mat2 = mat2 if isinstance(mat2[0], list) else [[mat2]*len(mat1[0])]*len(mat1)
     
-    return (add(mat1, mat2), sub(mat1, mat2), mul(mat1, mat2), div(mat1, mat2))
+    result = list(zip(*list(map(operate, mat1, mat2))))
+
+    return ([list(item) for item in result[0]],
+            [list(item) for item in result[1]],
+            [list(item) for item in result[2]],
+            [list(item) for item in result[3]])
 
 # Given code
 mat1 = [[11, 22, 33], [44, 55, 66]]
