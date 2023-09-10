93ade2f4bda37b6abe63f91e4c8fe6948853e8ff
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-10 11:55:16 +0300

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 def np_elementwise(mat1, mat2):
-    """Perform element-wise operations on mat1 and mat2."""
-    
-    # Broadcasting mat2 if it's a scalar.
-    mat2_broadcast = [[mat2] * len(mat1[0])] * len(mat1)
+    """
+    Perform element-wise operations on two matrices.
 
-    add = list(map(lambda x, y: list(map(lambda a, b: a + b, x, (y if isinstance(mat2[0], list) else mat2_broadcast))), mat1, mat2))
-    sub = list(map(lambda x, y: list(map(lambda a, b: a - b, x, (y if isinstance(mat2[0], list) else mat2_broadcast))), mat1, mat2))
-    mul = list(map(lambda x, y: list(map(lambda a, b: a * b, x, (y if isinstance(mat2[0], list) else mat2_broadcast))), mat1, mat2))
-    div = list(map(lambda x, y: list(map(lambda a, b: a / b, x, (y if isinstance(mat2[0], list) else mat2_broadcast))), mat1, mat2))
+    Args:
+    - mat1 (numpy.ndarray): First matrix
+    - mat2 (numpy.ndarray): Second matrix or scalar
 
-    return add, sub, mul, div
+    Returns:
+    - tuple: A tuple containing matrices resulting from element-wise sum, difference, product, and quotient
+    """
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)
 
-mat1 = [[11, 22, 33], [44, 55, 66]]
-mat2 = [[1, 2, 3], [4, 5, 6]]
+# Given code
+mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+mat2 = np.array([[1, 2, 3], [4, 5, 6]])
 
 print(mat1)
 print(mat2)
 add, sub, mul, div = np_elementwise(mat1, mat2)
 print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
-
 add, sub, mul, div = np_elementwise(mat1, 2)
 print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
