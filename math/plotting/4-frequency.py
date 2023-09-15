01baedf1553c39c3ebf3a3e6d8f7732de0e359e1
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-15 18:16:26 +0300

    Create 4-frequency.py

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# Create a histogram
+plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+
+# Add labels and title
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+
+# Show the plot
+plt.show()
