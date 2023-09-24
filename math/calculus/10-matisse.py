bcc360bce6c3751fbc4bfbbea1253ea737d4d28d
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-24 10:55:10 +0300

    Create 10-matisse.py

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,19 @@
+def poly_derivative(poly):
+    """
+    Calculates the derivative of a polynomial.
+
+    :param poly: List of coefficients representing a polynomial
+                 The index of the list represents the power of x that the coefficient belongs to
+    :type poly: list
+    :return: New list of coefficients representing the derivative of the polynomial
+    :rtype: list or None
+    """
+    if not isinstance(poly, list) or len(poly) == 0 or not all(isinstance(coef, (int, float)) for coef in poly):
+        return None
+    
+    if len(poly) == 1:
+        return [0]  # the derivative of a constant is 0
+    
+    derivative = [poly[i] * i for i in range(1, len(poly))]
+    
+    return derivative
