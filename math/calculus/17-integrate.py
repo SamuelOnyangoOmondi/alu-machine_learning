e5e8206f7041adb2d7a3d68bf682c2ed61a45bca
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-24 19:50:31 +0300

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python3
-def poly_integral(poly, C=0):
-    if not all(isinstance(coef, (int, float)) for coef in poly) or not isinstance(C, (int, float)):
-        return None
-    
-    if not poly:  # if the polynomial is empty, return None
-        return None
-    
-    integral = [C]  # start with the constant of integration
-    for i, coef in enumerate(poly):
-        if coef == 0:
-            integral.append(0)
-        else:
-            new_coef = coef / (i + 1)
-            # if the new coefficient is a whole number, represent it as an integer
-            if new_coef.is_integer():
-                new_coef = int(new_coef)
-            integral.append(new_coef)
-    
-    # Remove trailing zeros from the list of coefficients
-    while len(integral) > 1 and integral[-1] == 0:
-        integral.pop()
-    
-    return integral
-
-
-# Test the function with the provided example
+# Ensure that the poly is correctly initialized
 poly = [5, 3, 0, 1]
+
+# Call the function once and print the result
+# Ensure there is no extra call to this function with print statement
 print(poly_integral(poly))
+
+# If you want to check multiple cases, it should match the expected output
+print(poly_integral([0, 7, 2, 2, 0.25, 1]))
+print(poly_integral([5, 4, 4, 0.6666666666666666, 1, 1.4, 0.16666666666666666, 1.2857142857142858]))
