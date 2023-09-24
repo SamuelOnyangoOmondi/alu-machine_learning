eef63f4ead556bbb408673d843b224ab96d2257d
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-24 19:52:07 +0300

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-# Ensure that the poly is correctly initialized
-poly = [5, 3, 0, 1]
+def poly_integral(poly, C=0):
+    # Ensure this function is well defined before trying to print its result.
+    # Your implementation here.
+    pass
 
-# Call the function once and print the result
-# Ensure there is no extra call to this function with print statement
-print(poly_integral(poly))
-
-# If you want to check multiple cases, it should match the expected output
-print(poly_integral([0, 7, 2, 2, 0.25, 1]))
-print(poly_integral([5, 4, 4, 0.6666666666666666, 1, 1.4, 0.16666666666666666, 1.2857142857142858]))
+poly = [5, 3, 0, 1]  # Ensure poly is well defined before trying to print its result.
+print(poly_integral(poly))  # Now this should not raise NameError
