ac286169b41c789cf7cfdd2033b2d519f3cd8b6d
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-24 10:48:06 +0300

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
+"""
+This module contains a function that calculates the summation of i^2 from 1 to n
+"""
+
 def summation_i_squared(n):
+    """
+    Calculates the summation of i^2 from 1 to n
+    
+    :param n: stopping condition, must be a valid number
+    :type n: int or float
+    :return: Integer value of the sum or None if n is not a valid number
+    :rtype: int or None
+    """
     if not isinstance(n, (int, float)) or n < 1:
         return None
     if isinstance(n, float) and n.is_integer():
