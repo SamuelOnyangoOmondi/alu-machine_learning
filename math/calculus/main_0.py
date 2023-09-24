4e595dadb4a34cea40f08819a7152be5bba3e1e8
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-24 19:56:30 +0300

    Update main_0.py

--- math/calculus/main_0.py
+++ math/calculus/main_0.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
+from 17-integrate import poly_integral  # Ensure this import is correct
 
-# Importing the poly_integral function from the 17-integrate module
-from 17-integrate import poly_integral
-
-poly = [5, 3, 0, 1]  # example poly
-
-# Printing the result
-print(poly_integral(poly, C=0))  # Replace C=0 with your integration constant if different
-
-# Additional test cases
+# Your Test Cases
+print(poly_integral([5, 3, 0, 1], C=0))
 print(poly_integral([0, 7, 2, 2, 0.25, 1], C=1))
 print(poly_integral([5, 4, 4, 0.6666666666666666, 1, 1.4, 0.16666666666666666, 1.2857142857142858], C=0))
