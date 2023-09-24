6136e54a27a584e99dc218e405586c7d2e4b4bbf
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-24 11:03:02 +0300

    Update 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,9 +4,9 @@ def poly_derivative(poly):
     Calculates the derivative of a polynomial.
 
     :param poly: List of coefficients representing a polynomial.
-                 Index of the list represents the power of x that the coefficient belongs to.
+    Index of the list represents the power of x.
     :type poly: list
-    :return: New list of coefficients representing the derivative of the polynomial.
+    :return: New list of coefficients for coefficients.
     :rtype: list or None
     """
     if not isinstance(poly, list) or len(poly) == 0 or not all(
