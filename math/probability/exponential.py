a6a125c12b9dc168e6ebf47a1616099d6c486791
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-09-28 13:13:46 +0300

    Update exponential.py

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python3
+"""
+This class represents a exponential distribution
+"""
+
+
 class Exponential:
+    """
+    This class represents a exponential distribution
+    """
+
     def __init__(self, data=None, lambtha=1.):
+        """
+        function initializes the exponential distribution
+        data - list of the data to be used to estimate the distribution
+        lambtha - expected number of occurences in a given time frame
+        """
+
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
+            else:
+                self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = 1 / (sum(data) / len(data))  # Calculate lambtha from data
+            self.lambtha = float(1 / (sum(data) / len(data)))
 
-if __name__ == "__main__":
-    # Simulate data using a simple random method (for demonstration)
-    import random
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given time period
+        x = time period
+        """
+        if x < 0:
+            return 0
+        return self.lambtha * 2.7182818285 ** (-self.lambtha * x)
 
-    random.seed(0)
-    data = [random.expovariate(0.5) for _ in range(100)]
-    e1 = Exponential(data)
-    print('Lambtha:', e1.lambtha)
-
-    e2 = Exponential(lambtha=2)
-    print('Lambtha:', e2.lambtha)
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given time period
+        x = time period
+        """
+        if x < 0:
+            return 0
+        return 1 - 2.7182818285 ** (-self.lambtha * x)
