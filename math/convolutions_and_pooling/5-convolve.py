900827af4e082014b4cab8520c53258238df0f39
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-10-06 14:09:11 +0300

    Update 5-convolve.py

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -1,52 +1,38 @@
 #!/usr/bin/env python3
 
+
+import numpy as np
 """
 This module has the method that performs
 convolution on grayscale images
 with multiple kernels
 """
-import numpy as np
-
-
 def convolve(images, kernels, padding='same', stride=(1, 1)):
-    """
-    Convolution with multiple kernels
-    c - no. of channels
-    nc - no. of kernels/ filters
-    """
-    m, h, w, c = images.shape
-    kh, kw, _, nc = kernels.shape
-    # stride
+    m, h, w, _ = images.shape
+    fh, fw, _, c = kernels.shape
     sh, sw = stride
 
     if padding == 'same':
-        ph = max(0, (h - 1) * stride[0] + kh - h)
-        pw = max(0, (h-1) * stride[1] + kw - w)
+        nh, nw = h, w
+        ph  = int((fh + (sh * (h - 1)) - h) / 2 + 0.5)
+        pw  = int((fw + (sw * (w - 1)) - w) / 2 + 0.5)
+        images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
     elif padding == 'valid':
-        ph, pw = 0, 0
-    elif isinstance(padding, tuple):
+        nh = ((h - fh) // sh) + 1
+        nw = ((w - fw) // sw) + 1
+    else:
         ph, pw = padding
-
-    # Calculate the output dimensions
-    oh = int((h - kh + 2 * ph) / sh) + 1
-    ow = int((w - kw + 2 * pw) / sw) + 1
-
-    # Pad the input images
-    images_padded = np.pad(
-        images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
-
-    # Initialize the output tensor
-    convolved_images = np.zeros((m, oh, ow, nc))
-
-    # Perform the convolution
-    for i in range(oh):
-        for j in range(ow):
-            for k in range(nc):
-                # Extract a patch from the image
-                patch = images_padded[:, i * sh:i *
-                                      sh + kh, j * sw:j * sw + kw, :]
-                # Convolve using tensordot to handle multiple channels
-                convolved_images[:, i, j, k] = np.tensordot(
-                    patch, kernels[:, :, :, k], axes=([1, 2, 3], [0, 1, 2]))
-
-    return convolved_images
+        nh = ((h - fh + (2 * ph)) // sh) + 1
+        nw = ((w - fw + (2 * pw)) // sw) + 1
+        images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
+    convolution = np.zeros((m, nh, nw, c))
+    for i in range(nh):
+        h_start = i * sh
+        h_end = h_start + fh
+        for j in range(nw):
+            w_start = j * sw
+            w_end = w_start + fw
+            images_slice = images[:, h_start:h_end, w_start:w_end]
+            for k in range(c):
+                convolution[:, i, j, k] = np.sum(images_slice * kernels[:, :, :, k], axis=(1, 2, 3))
+    return convolution
