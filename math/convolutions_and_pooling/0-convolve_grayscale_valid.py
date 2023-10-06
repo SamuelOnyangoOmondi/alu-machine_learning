0555725db6bd4f046706db54c28964f8b2ff0b99
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-10-06 14:39:35 +0300

    Update 0-convolve_grayscale_valid.py

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,41 +1,20 @@
 #!/usr/bin/env python3
 
-"""
-This module has the method that performs a valid
-convolution on grayscale images
-"""
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
-    """
-    perfom a concolution of a grayscale image
-    images is a np.ndarray with shape(m, h, w)
-    m - no. of images
-    h - height in pixels of images
-    w - width in pixels of images
-    kernel is nmpy.ndarray with shape(kh, kw)
-    kh - height of kernel
-    kw - width of kernel
-    Returns: numpy.ndarray containing the convolved images
-    """
     m, h, w = images.shape
-    kh, kw = kernel.shape
+    fh, fw = kernel.shape
 
-    # Calculate the output size for "valid" convolution
-    out_h = h - kh + 1
-    out_w = w - kw + 1
+    h = h - fh + 1
+    w = w - fw + 1
+    convolution = np.zeros((m, h, w))
 
-    # Initialize the output array
-    convolved_images = np.zeros((m, out_h, out_w))
-
-    # Perform "valid" convolution with two for loops
-    for i in range(out_h):
-        for j in range(out_w):
-            # Extract a patch from the image
-            patch = images[:, i:i + kh, j:j + kw]
-            # Perform element-wise multiplication and sum
-            convolved_images[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
-            print(convolved_images)
-
-    return convolved_images
+    for i in range(h):
+        h_start, h_end = i, i + fh
+        for j in range(w):
+            w_start, w_end = j, j + fw
+            images_slice = images[:, h_start:h_end, w_start:w_end]
+            convolution[:,i,j] = np.sum(images_slice * kernel, axis=(1, 2))
+    return convolution
