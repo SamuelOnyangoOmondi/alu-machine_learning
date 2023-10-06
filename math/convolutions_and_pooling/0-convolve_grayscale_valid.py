26246ee52682678cd6bc857ae6bd47dfece785b3
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-10-06 11:23:30 +0300

    Update 0-convolve_grayscale_valid.py

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -4,6 +4,16 @@ import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
+    """
+    Perform a valid convolution on grayscale images using the given kernel.
+
+    Args:
+        images (numpy.ndarray): Input grayscale images with shape (m, h, w).
+        kernel (numpy.ndarray): Convolution kernel with shape (fh, fw).
+
+    Returns:
+        numpy.ndarray: Convolved images with shape (m, h - fh + 1, w - fw + 1).
+    """
     m, h, w = images.shape
     fh, fw = kernel.shape
 
@@ -16,5 +26,16 @@ def convolve_grayscale_valid(images, kernel):
         for j in range(w):
             w_start, w_end = j, j + fw
             images_slice = images[:, h_start:h_end, w_start:w_end]
-            convolution[:,i,j] = np.sum(images_slice * kernel, axis=(1, 2))
+            convolution[:, i, j] = np.sum(images_slice * kernel, axis=(1, 2))
     return convolution
+
+if __name__ == '__main__':
+    # Example usage:
+    dataset = np.load('../../supervised_learning/data/MNIST.npz')
+    images = dataset['X_train']
+    print("Input images shape:", images.shape)
+    
+    kernel = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]])
+    images_conv = convolve_grayscale_valid(images, kernel)
+    
+    print("Convolved images shape:", images_conv.shape)
