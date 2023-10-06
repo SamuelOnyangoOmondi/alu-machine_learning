7f98a2c0afb86434eaad2574707424c59befb196
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-10-06 14:25:34 +0300

    Update 4-convolve_channels.py

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python3
 
-"""
-This module has the method that performs
-convolution on grayscale images with channels
-"""
 import numpy as np
 
 
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
-    """
-    convolution with channels
-    """
-    m, h, w, c = images.shape
-    kh, kw, kc = kernel.shape
-    # stride
+    m, h, w, _ = images.shape
+    fh, fw, _ = kernel.shape
     sh, sw = stride
 
     if padding == 'same':
-        ph = int(((h - 1) * sh + kh - h) / 2)
-        pw = int(((w - 1) * sw + kw - w) / 2)
-    elif isinstance(padding, tuple):
-        ph, pw = padding
+        nh, nw = h, w
+        ph = int((fh + (sh * (h - 1)) - h) / 2 + 0.5)
+        pw = int((fw + (sw * (w - 1)) - w) / 2 + 0.5)
+        images = np.pad(images,
+                        ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
     elif padding == 'valid':
-        ph, pw = 0, 0
-
-    # output
-    out_h = int((h - kh + 2 * ph) / sh) + 1
-    out_w = int((w - kw + 2 * pw) / sw) + 1
-
-    # Pad the input images
-    images_padded = np.pad(
-        images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
-
-    # initialise convolved images
-    convolved_images = np.zeros((m, out_h, out_w))
-
-    # Perform the convolution
-    for i in range(out_h):
-        for j in range(out_w):
-            # Extract a patch from the image
-            patch = images_padded[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
-            convolved_images[:, i, j] = np.tensordot(
-                patch, kernel, axes=([1, 2, 3], [0, 1, 2]))
-
-    return convolved_images
+        nh = ((h - fh) // sh) + 1
+        nw = ((w - fw) // sw) + 1
+    else:
+        ph, pw = padding
+        nh = ((h - fh + (2 * ph)) // sh) + 1
+        nw = ((w - fw + (2 * pw)) // sw) + 1
+        images = np.pad(images,
+                        ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
+    convolution = np.zeros((m, nh, nw))
+    for i in range(nh):
+        h_start = i * sh
+        h_end = h_start + fh
+        for j in range(nw):
+            w_start = j * sw
+            w_end = w_start + fw
+            images_slice = images[:, h_start:h_end, w_start:w_end]
+            convolution[:, i, j] = np.sum
+            (images_slice * kernel, axis=(1, 2, 3))
+    return convolution
