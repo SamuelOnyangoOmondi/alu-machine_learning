2a432fa911c2447eb0656640eba0c3888a903f9f
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-01-17 10:05:42 +0300

    Update 0-neuron.py

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -3,17 +3,6 @@ import numpy as np
 
 
 class Neuron:
-    """
-    Represents a single neuron performing binary classification.
-
-    Attributes:
-    W (numpy.ndarray): The weights vector for the neuron. Initialized with a random normal distribution.
-    b (float): The bias for the neuron. Initialized to 0.
-    A (int): The activated output of the neuron (prediction). Initialized to 0.
-
-    Methods:
-    __init__(self, nx): Initializes the neuron with nx input features.
-    """
     def __init__(self, nx):
         if type(nx) != int:
             raise TypeError("nx must be an integer")
@@ -23,6 +12,7 @@ class Neuron:
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
+
 
 # Usage example
 if __name__ == "__main__":
