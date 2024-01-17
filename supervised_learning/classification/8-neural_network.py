5d31a88ce80781b9654f8cd228373aae0019e969
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-01-17 23:59:08 +0300

    Create 8-neural_network.py

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self, nx, nodes):
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
+
+# Usage example
+if __name__ == "__main__":
+    np.random.seed(0)
+    nn = NeuralNetwork(784, 3)
+    print(nn.W1)
+    print(nn.W1.shape)
+    print(nn.b1)
+    print(nn.W2)
+    print(nn.W2.shape)
+    print(nn.b2)
+    print(nn.A1)
+    print(nn.A2)
+    nn.A1 = 10  # This will work but it's generally not good practice to modify attributes directly
+    print(nn.A1)
