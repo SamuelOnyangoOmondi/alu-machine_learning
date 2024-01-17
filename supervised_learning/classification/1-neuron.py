3b63a2e44ccaff9f394a3ddff940816fa76720f2
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-01-17 10:29:47 +0300

    Create 1-neuron.py

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+class Neuron:
+    def __init__(self, nx):
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)  # Private attribute
+        self.__b = 0                        # Private attribute
+        self.__A = 0                        # Private attribute
+
+    @property
+    def W(self):
+        """ Getter for private attribute __W """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter for private attribute __b """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter for private attribute __A """
+        return self.__A
+
+# Usage example
+if __name__ == "__main__":
+    np.random.seed(0)
+    neuron = Neuron(784)
+    print(neuron.W)
+    print(neuron.b)
+    print(neuron.A)
+    
+    # The following line will raise an AttributeError, because __A is private and has no setter
+    # neuron.A = 10
