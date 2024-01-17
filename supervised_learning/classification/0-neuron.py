0199856123c4bb4a16b6ed4f21145b97c99a6a60
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-01-17 09:54:15 +0300

    Update 0-neuron.py

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import numpy as np
 
+
 class Neuron:
     def __init__(self, nx):
         if type(nx) != int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
+
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
+
 
 # Usage example
 if __name__ == "__main__":
