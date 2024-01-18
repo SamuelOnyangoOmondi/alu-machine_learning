a960e6d3e864cdb2c941cb6d4d02a5b3f0716ad6
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-01-18 20:21:26 +0300

    Update 5-main.py

--- supervised_learning/classification/5-main.py
+++ supervised_learning/classification/5-main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 Neuron = __import__('5-neuron').Neuron
 
-lib_train = np.load('./data/Binary_Train.npz')
+lib_train = np.load('../data/Binary_Train.npz')
 X_3D, Y = lib_train['X'], lib_train['Y']
 X = X_3D.reshape((X_3D.shape[0], -1)).T
 
@@ -14,3 +14,4 @@ A = neuron.forward_prop(X)
 neuron.gradient_descent(X, Y, A, 0.5)
 print(neuron.W)
 print(neuron.b)
+# print()
