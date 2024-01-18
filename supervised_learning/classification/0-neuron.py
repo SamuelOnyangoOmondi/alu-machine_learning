7d2e624b5ce46c0a3237b2427d76236e0c70d4cd
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-01-18 20:13:22 +0300

    Update 0-neuron.py

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-"""This module is of a single perfoming classification"""
+"""Class Neuron that defines a single neuron performing binary classification
+"""
+
+
 import numpy as np
 
 
 class Neuron:
-    """class that defines a single neuron performing classification"""
+    """ Class Neuron
+    """
 
     def __init__(self, nx):
-        """class constructor
+        """ Instantiation function of the neuron
+
+        Args:
+            nx (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
         """
-        # nx - no. of input features to the neuron
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise TypeError('nx must be an integer')
+        if nx < 0:
+            raise ValueError('nx must be positive')
 
-        # Mean and standard deviation
-        mean = 0
-        std_dev = 1
-
-        # Generate a random number from a normal distribution
-        self.W = np.random.normal(0, 1, (nx, 1))
-
-        # b - bias of the neuron
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
-
-        # A - activated output of the neuron(Prediction)
         self.A = 0
