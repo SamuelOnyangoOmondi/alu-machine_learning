897a0af5d2dde9d50d78c3e166398d0a3187d1ef
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-01-18 20:25:14 +0300

    Update 8-neural_network.py

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
+""" Neural Network
+"""
+
 import numpy as np
 
+
 class NeuralNetwork:
+    """ Class that defines a neural network with one hidden layer performing
+        binary classification.
+    """
+
     def __init__(self, nx, nodes):
-        if type(nx) != int:
-            raise TypeError("nx must be an integer")
+        """ Instantiation function
+
+        Args:
+            nx (int): size of the input layer
+            nodes (_type_): _description_
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if type(nodes) != int:
-            raise TypeError("nodes must be an integer")
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
         if nodes < 1:
-            raise ValueError("nodes must be a positive integer")
+            raise ValueError('nodes must be a positive integer')
 
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
@@ -18,20 +33,3 @@ class NeuralNetwork:
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
-
-# Usage example
-if __name__ == "__main__":
-    np.random.seed(0)
-    nn = NeuralNetwork(784, 3)
-    print(nn.W1)
-    print(nn.W1.shape)
-    print(nn.b1)
-    print(nn.W2)
-    print(nn.W2.shape)
-    print(nn.b2)
-    print(nn.A1)
-    print(nn.A2)
-    # Breaking the following comment into two lines to adhere to PEP 8
-    # This will work but it's generally not good practice to modify attributes directly
-    nn.A1 = 10
-    print(nn.A1)
