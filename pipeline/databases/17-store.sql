b7ffd51f13cb1c74e24ee2eb34b49dcfdfcc3121
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-03-20 09:42:23 +0300

    Update 17-store.sql

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,13 +1,14 @@
--- first trigger
-DELIMETER //
+-- Creates a trigger that decreases the quantity of an item after adding a new order
+-- quantity in the table `items` can be negative
+DROP TRIGGER IF EXISTS decrease_quantity;
+
+DELIMITER $$
 CREATE TRIGGER decrease_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
+       AFTER INSERT
+       ON `orders` FOR EACH ROW
 BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE id = NEW.item_name;
-END;
-//
-DELIMETER ;
-ON items
+        UPDATE items SET quantity = quantity - new.number
+        WHERE items.name=new.item_name;
+END $$
+
+DELIMITER ;
