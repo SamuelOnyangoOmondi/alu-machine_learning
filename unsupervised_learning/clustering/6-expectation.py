c48a2223b7ff94f68aa8d91804051469350a6f9f
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2024-07-10 17:28:52 +0300

    Update 6-expectation.py

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,51 +1,52 @@
 #!/usr/bin/env python3
-
 """
-This module contains a function that calculates
-expectation step in the EM algorithm for a GMM
+Calculates the expectation step in the EM algorithm for a GMM
 """
-
 import numpy as np
 pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
     """
-    initializes variables for a Gaussian Mixture Model
-
-    X: numpy.ndarray (n, d) containing the dataset
-        - n no. of data points
-        - d no. of dimensions for each data point
-    pi: numpy.ndarray (k,) containing the priors for each cluster
-    m: numpy.ndarray (k, d) containing centroid means for each cluster
-    S: numpy.ndarray (k, d, d) covariance matrices for each cluster
-
-    return:
-        - g: numpy.ndarray (k, n) containing the posterior
-            probabilities for each data point in each cluster
-        -l: log likelihood of the model
+    Calculates the expectation step in the EM algorithm for a GMM
+    :param X: numpy.ndarray of shape (n, d) containing the data set
+    :param pi: numpy.ndarray of shape (k,) containing the priors for each
+    cluster
+    :param m: numpy.ndarray of shape (k, d) containing the centroid means for
+    each cluster
+    :param S: numpy.ndarray of shape (k, d, d) containing the covariance
+    matrices for each cluster
+    :return: g, l, or None, None on failure
+        g is a numpy.ndarray of shape (k, n) containing the posterior
+        probabilities for each data point in each cluster
+        l is the total log likelihood
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
-    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+    if type(pi) is not np.ndarray or len(pi.shape) != 1:
         return None, None
-    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+    if type(m) is not np.ndarray or len(m.shape) != 2:
         return None, None
-    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+    if type(S) is not np.ndarray or len(S.shape) != 3:
         return None, None
-    n, d = X.shape
     k = pi.shape[0]
+    n, d = X.shape
+
+    if k > n:
+        return None, None
     if d != m.shape[1] or d != S.shape[1] or d != S.shape[2]:
         return None, None
     if k != m.shape[0] or k != S.shape[0]:
         return None, None
-    if not np.isclose(np.sum(pi), 1):
+    if not np.isclose([np.sum(pi)], [1])[0]:
         return None, None
-    if not all(np.isclose(np.sum(pi), 1) for pi in S):
-        return None, None
-    g = np.zeros((k, n))
+
+    probs = np.zeros((k, n))
     for i in range(k):
-        g[i] = pi[i] * pdf(X, m[i], S[i])
-    g = g / np.sum(g, axis=0)
-    l = np.sum(np.log(np.sum(g, axis=0)))
-    return g, l
+        probs[i] = pi[i] * pdf(X, m[i], S[i])
+
+    marginal = np.sum(probs, axis=0)
+    g = probs / marginal
+    log_likelihood = np.sum(np.log(marginal))
+
+    return g, log_likelihood
